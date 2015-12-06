df815215526d0623c4509cd21390e17cef2e0677
Elliot Plant <elliotaplant@gmail.com>
2015-12-05 23:17:10 -0800

    Implemented getElementsByClassName and passed all tests
    Had to use a second parameter to tell whether the current
    call was the first call. This could possibly be removed
    if the function was added to the object prototype...

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
-};
+
+var getElementsByClassName = function(className, inside){
+
+  // recursively call itself to make sure document is top level "this"
+  if (!inside) { // first call should be to #document
+    return getElementsByClassName.apply(document.body, [className, true]);
+  }
+  var out = [];
+  // use String includes function, not supported by IE or Opera
+  if (this.className && this.className.includes(className)) {
+    out.push(this);
+  }
+
+  // recursively call on the child nodes of the current node
+  _.each(this.childNodes, function(element) {
+    out = out.concat(getElementsByClassName.apply(element, [className, true]));
+  })
+  return out;
+}
